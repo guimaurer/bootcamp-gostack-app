2fa81a19593e97e0e104c54fe958351c02f37118
guimaurer <guilhermemaurer@hotmail.com>
2019-10-03 09:09:12 -0300

    crate model

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,6 @@ import { Router } from 'express';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ mensagem: 'ola gui' }))
+routes.get('/', (req, res) => res.json({ mensagem: 'ola gui' }));
 
 export default routes;
