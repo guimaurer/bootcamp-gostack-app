03fedad5c9c8a717d32fc25ac5a8c900eea78e95
guimaurer <guilhermemaurer@hotmail.com>
2019-10-08 20:31:07 -0300

    fim do exercicio 2

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: 'b715213c7d54d2c51d5546058e6cd490',
+  expiresIn: '7d',
+};
--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,13 @@
 import { Router } from 'express';
 
-const routes = new Router();
+import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
 
-routes.get('/', (req, res) => res.json({ mensagem: 'ola gui' }));
+import authMiddleware from './app/middlewares/auth';
+
+const routes = new Router();
+routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
+routes.put('/users', authMiddleware, UserController.update);
 
 export default routes;
